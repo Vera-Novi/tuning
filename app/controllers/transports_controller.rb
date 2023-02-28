f91fa43f77d60b08a1431dcb72b7b94ae0319bcd
Veronica Novikova <veronica.novikova@gmail.com>
2023-02-28 19:32:43 +0100

    new

--- app/controllers/transports_controller.rb
+++ app/controllers/transports_controller.rb
@@ -12,16 +12,13 @@ class TransportsController < ApplicationController
   end
 
   def create
-    @transport = Transport.new(transport_params)
-<<<<<<< HEAD
-=======
+
     @transport.user_id = current_user.id
     if @transport.save
       redirect_to transport_path(@transport)
     else
       render :new, status: :unprocessable_entity
     end
->>>>>>> 70ae070707471452c27356031f1eb24e55d50884
   end
 
   private
