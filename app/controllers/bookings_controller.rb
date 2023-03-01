eb174a680e4b93ef6027c14b20cec522c835b7c5
jean-francois fays <fays.jf@gmail.com>
2023-03-01 17:03:50 +0100

    booking_crud

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,31 @@
+class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @transport = Transport.find(params[:transport_id])
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.transport = Transport.find(params[:transport_id])
+    @booking.user_id = current_user.id
+    @booking.save
+    # No need for app/views/bookings/create.html.erb
+    redirect_to booking_path(@booking)
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to transport_path(@booking.transport), status: :see_other
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:reservation_begin, :reservation_end, :user_id, :transport_id)
+  end
+end
