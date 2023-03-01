74f64493712a1d8bbf5625457a9b2bfbe07045df
Veronica Novikova <veronica.novikova@gmail.com>
2023-03-01 15:07:52 +0100

    test seeds merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_03_01_102213) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,11 +57,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_102213) do
     t.string "brand"
     t.string "horse"
     t.integer "year"
-    t.bigint "user_id", null: false
+    t.bigint "user_transport_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price"
-    t.index ["user_id"], name: "index_transports_on_user_id"
+    t.index ["user_transport_id"], name: "index_transports_on_user_transport_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -84,5 +82,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_102213) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "transports"
   add_foreign_key "bookings", "users"
-  add_foreign_key "transports", "users"
+  add_foreign_key "transports", "users", column: "user_transport_id"
 end
