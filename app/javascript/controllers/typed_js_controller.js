f8df8e2fe6d16cb6cca7c4afc224e1585d396cd6
Marc Bayle <marc.scl.bayle@gmail.com>
2023-03-02 11:32:19 +0100

    change

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -4,7 +4,7 @@ import Typed from "typed.js"
 export default class extends Controller {
   connect() {
     new Typed(this.element, {
-      strings: ["KEKE Transports", "moving you around","with", "exotic style","Make your journey unforgetable !"],
+      strings: ["KEKE Transports", "moving you around", "with exotic style","Make your journey unforgetable !"],
       typeSpeed: 50,
       loop: true
     })
