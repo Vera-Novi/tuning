65ddf19d1b544ceb7e76c098053ceb36c8d542cd
jean-francois fays <fays.jf@gmail.com>
2023-03-03 14:27:31 +0100

    validates / sign up buttons fix

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :transport
+
+  validates :reservation_end, comparison: { greater_than: :reservation_begin }
+  # validates_comparison_of :reservation_begin, less_than: :reservation_end, greater_than: -> { Date.today }
 end
